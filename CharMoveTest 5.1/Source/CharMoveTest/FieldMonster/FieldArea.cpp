687b98c0df63e35af51a983168db46ff3e7242dc
TheLeafeon <zkjx12@gmail.com>
2023-04-10 19:58:07 +0900

    MoveBasket
    움직이는 바구니

--- CharMoveTest 5.1/Source/CharMoveTest/FieldMonster/FieldArea.cpp
+++ CharMoveTest 5.1/Source/CharMoveTest/FieldMonster/FieldArea.cpp
@@ -4,6 +4,8 @@
 #include "CharMoveTest/FieldMonster/FieldArea.h"
 #include "DrawDebugHelpers.h"
 #include "Components/BoxComponent.h"
+#include "Engine/EngineTypes.h"
+
 #include "Engine/World.h"
 #include "Engine.h"
 
@@ -45,7 +47,7 @@ void AFieldArea::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* Oth
 	{
 		firstIn = true;
 
-		//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Green, TEXT("Overlap"));
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Green, TEXT("Overlap"));
 
 
 		if (AreaSpawnMonsterArray.Num() == 0)
---
+++ CharMoveTest 5.1/Source/CharMoveTest/Interaction/Basket.h
@@ -0,0 +1,59 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Basket.generated.h"
+
+UCLASS()
+class CHARMOVETEST_API ABasket : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABasket();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UBoxComponent* PlayerSit;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector BasketOverlapBoxSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector TargetActorLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector StudioTargetActorLocation;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//Basket 이동
+	//UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void MoveBasket();
+
+	//공격 후 다시 움직이도록 변수 변경 하는 함수 BP에서 정의
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void StudioTeleport();
+
+	UPROPERTY(EditAnywhere, Category = "TargetLocation")
+	class AActor* TargetActor;
+
+	UPROPERTY(EditAnywhere, Category = "StudioTargetLocation")
+	class AActor* StudioTargetActor;
+
+	FTimerHandle BasketMoveHandle;
+
+private:
+	bool firstIn;
+
+};
