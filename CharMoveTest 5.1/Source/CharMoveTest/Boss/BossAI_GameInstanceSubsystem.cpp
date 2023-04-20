536d181f82021b02840edf6a98dec86fab7bdf99
박연지 <67936804+prg217@users.noreply.github.com>
2023-04-20 22:57:20 +0900

    곰인형 보스 애니메이션 체인지 및 파티클 적용(미완)
    돌진 전조, 회전 전조 변경
    파티클 적용(바꿔야함)

--- CharMoveTest 5.1/Source/CharMoveTest/Boss/BossAI_GameInstanceSubsystem.cpp
+++ CharMoveTest 5.1/Source/CharMoveTest/Boss/BossAI_GameInstanceSubsystem.cpp
@@ -3,7 +3,7 @@
 
 #include "CharMoveTest/Boss/BossAI_GameInstanceSubsystem.h"
 
-UBossAI_GameInstanceSubsystem::UBossAI_GameInstanceSubsystem() : Control_DetectRadius(800.0f), BearDoll_RushSpeed(1000.0f), RotationSpeed(2.0f), Rush_Delay(3.0f), Rotation_Delay(3.0f),
+UBossAI_GameInstanceSubsystem::UBossAI_GameInstanceSubsystem() : Control_DetectRadius(800.0f), BearDoll_RushSpeed(1000.0f), RotationSpeed(2.0f), Rush_Delay(3.0f),
 Fall_Speed(1.0f), Fall_Acceleration(1.4f), Fall_time(1.0f), Fall_Delay(1.5f), PD_Hit_Delay(4.0f)
 {
 
