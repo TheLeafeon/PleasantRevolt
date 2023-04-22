45705736f3c1ae03fff5262bb895b3bf8bcdc368
TheLeafeon <zkjx12@gmail.com>
2023-04-22 17:18:42 +0900

    MonsterDeath
    몬스터 데스 수정

--- CharMoveTest 5.1/Source/CharMoveTest/FieldMonster/SheepDoll.h
+++ CharMoveTest 5.1/Source/CharMoveTest/FieldMonster/SheepDoll.h
@@ -80,6 +80,10 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
 	void SheepDollKnockBack();
 
+	//죽었을때 더 멀리 넉백되는 함수 BP에서 정의
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SheepDollDeathKnockBack();
+
 	//죽고 사라지는 시간
 	void DeathTimer();
 
