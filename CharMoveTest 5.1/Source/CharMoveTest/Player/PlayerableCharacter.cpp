f0c9c03f1388fb463c8d4a29fab5df5d067c0425
TheLeafeon <zkjx12@gmail.com>
2023-05-02 12:38:45 +0900

    Stage1_2InteractObject
    2구간 상호작용액터 배치

--- CharMoveTest 5.1/Source/CharMoveTest/Player/PlayerableCharacter.cpp
+++ CharMoveTest 5.1/Source/CharMoveTest/Player/PlayerableCharacter.cpp
@@ -444,7 +444,7 @@ void APlayerableCharacter::DeathEnd()
 	//this->SetActorHiddenInGame(this);
 	//SetLifeSpan(0.1f);
 	GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, TEXT("PlayerDeathEnd"));
-	PlayerDeathLevelReStart();
+	//PlayerDeathLevelReStart();
 }
 
 //=============== Weapon & Switching System =============== //
