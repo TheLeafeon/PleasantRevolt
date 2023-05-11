7270f14ef56d3dfe962a32a6cd414ab7fa765e5c
박연지 <67936804+prg217@users.noreply.github.com>
2023-05-12 00:09:10 +0900

    피규어 보스 팔 뿌서짐

--- CharMoveTest 5.1/Source/CharMoveTest/Boss/PD_BackSmash_BTTaskNode.cpp
+++ CharMoveTest 5.1/Source/CharMoveTest/Boss/PD_BackSmash_BTTaskNode.cpp
@@ -5,19 +5,37 @@
 
 UPD_BackSmash_BTTaskNode::UPD_BackSmash_BTTaskNode() : RandomValue(0)
 {
-	NodeName = TEXT("PD BackSmash");
+	NodeName = TEXT("BackSmash");
 }
 
 EBTNodeResult::Type UPD_BackSmash_BTTaskNode::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	RandomValue = OwnerComp.GetBlackboardComponent()->GetValueAsFloat(bossKey2::randomArm);
 
-	if (RandomValue <= 0.5)
+	if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm) == nullptr && OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::rightArm) == nullptr)
+	{
+		//3페로 넘어가기
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(bossKey2::isArmLive, false);
+
+		return EBTNodeResult::Succeeded;
+	}
+	else if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm) == nullptr)
+	{
+		RandomValue = 0.6;
+		return EBTNodeResult::Succeeded;
+	}
+	else if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::rightArm) == nullptr)
+	{
+		RandomValue = 0.1;
+		return EBTNodeResult::Succeeded;
+	}
+
+	if (RandomValue <= 0.5 && OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm) != nullptr)
 	{
 		LArm = Cast<APD_LeftArm>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm));
 		LArm->BackSmash();
 	}
-	else
+	else if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::rightArm) != nullptr)
 	{
 		RArm = Cast<APD_RightArm>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::rightArm));
 		RArm->BackSmash();
--- CharMoveTest 5.1/Source/CharMoveTest/Boss/PD_Smash_BTTaskNode.cpp
+++ CharMoveTest 5.1/Source/CharMoveTest/Boss/PD_Smash_BTTaskNode.cpp
@@ -19,21 +19,24 @@ EBTNodeResult::Type UPD_Smash_BTTaskNode::ExecuteTask(UBehaviorTreeComponent& Ow
 		//3페로 넘어가기
 		OwnerComp.GetBlackboardComponent()->SetValueAsBool(bossKey2::isArmLive, false);
 
-		return EBTNodeResult::Failed;
+		return EBTNodeResult::Succeeded;
 	}
 	else if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm) == nullptr)
 	{
 		RandomValue = 0.6;
+		return EBTNodeResult::Succeeded;
 	}
 	else if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::rightArm) == nullptr)
 	{
 		RandomValue = 0.1;
+		return EBTNodeResult::Succeeded;
 	}
 
 	if (RandomValue <= 0.5 && OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm) != nullptr)
 	{
 		LArm = Cast<APD_LeftArm>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::leftArm));
 		LArm->Smash();
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("?"));
 	}
 	else if (OwnerComp.GetBlackboardComponent()->GetValueAsObject(bossKey2::rightArm) != nullptr)
 	{
