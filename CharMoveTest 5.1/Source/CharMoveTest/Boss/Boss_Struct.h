6e0a986c2ec0a7fe2ddf1b4d1fbfd5f5d7b38f30
박연지 <67936804+prg217@users.noreply.github.com>
2025-03-05 15:06:36 +0900

    Boss_Struct로 교체(진행 중)
    1. UBossAI_GameInstanceSubsystem대신 Boss_Struct로 교체(최종 확인 필요)
     - BearDollBass의 회전 공격 때 플레이어를 쫓아오지 않는 오류 해결 : Follow Pawn의 Info 구조체가 BearDollInfo가 아니었음
     - PlasticDollBoss의 테스트 필요 및 더 효율적인 방법은 없나 보기(하나의 구조체만을 사용하는 것이 아닌 분산되어 있음)

--- CharMoveTest 5.1/Source/CharMoveTest/Boss/Boss_Struct.h
+++ CharMoveTest 5.1/Source/CharMoveTest/Boss/Boss_Struct.h
@@ -29,19 +29,19 @@ struct FBearDollInfo
 	GENERATED_USTRUCT_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float Control_DetectRadius; //범위 반지름
+	float Control_DetectRadius = 500.f; //범위 반지름
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float BearDoll_RushSpeed;
+	float BearDoll_RushSpeed = 1000.f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float RotationSpeed;
+	float RotationSpeed = 2.f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float Rush_Delay;
+	float Rush_Delay = 3.f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float Rotation_Delay;
+	float Rotation_Delay = 3.f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float Follow_Speed;
+	float Follow_Speed = 350.f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BearDoll")
-	float Follow_Time;
+	float Follow_Time = 3.f;
 };
 
 USTRUCT(Atomic, BlueprintType)
